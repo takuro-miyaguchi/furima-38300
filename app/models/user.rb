28378c0206990cc72a079a20c84bd057bbcfff7e
trackmaintenance <trackmainte@gmail.com>
2022-08-26 16:02:03 +0900

    Issue4_ユーザー管理機能まで

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :purchase_records
+
+  validates :nickname,        presence: true
+  validates :email,          {presence: true, uniqueness: { case_sensitive: false }}
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name,       presence: true
+    validates :first_name,      presence: true
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' } do
+    validates :last_name_kana,  presence: true
+    validates :first_name_kana, presence: true
+  end
+
+end
