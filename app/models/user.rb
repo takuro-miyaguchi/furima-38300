4ed03e83a6a77563a0e329a9df7bfc3123066da7
trackmaintenance <trackmainte@gmail.com>
2022-08-27 21:16:53 +0900

    Issue4_テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,20 +7,21 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchase_records
 
-  validates :nickname,        presence: true
-  validates :email,          {presence: true, uniqueness: { case_sensitive: false }}
+  validates :nickname,          presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角文字を使用してください' } do
     validates :last_name,       presence: true
     validates :first_name,      presence: true
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'には全角カタカナを使用してください' } do
     validates :last_name_kana,  presence: true
     validates :first_name_kana, presence: true
   end
 
+  validates :birth_date,        presence: true
+  
 end
