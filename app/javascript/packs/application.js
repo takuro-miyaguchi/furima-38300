81bf72084db1dbf8dca930b58a02ea313387d7d4
trackmaintenance <trackmainte@gmail.com>
2022-08-31 20:14:54 +0900

    Issue5_js読み込みの修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,12 +16,4 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-window.addEventListener('load', () => {
-  const priceInput = document.getElementById("item-price");
-   priceInput.addEventListener("input", () => {
-     const addTaxDom = document.getElementById("add-tax-price");
-     addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1 );
-     const addPofitDom = document.getElementById("profit");
-     addPofitDom.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1 ))
- })
-});
+require("item_price");
